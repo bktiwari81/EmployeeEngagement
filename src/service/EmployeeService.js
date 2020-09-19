876a8779a0325b1869da259d997cacd46ca883df
Ashish <ashish2203@live.com>
2020-09-19 19:09:08 +0530

    Added Email Functinality

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -44,3 +44,31 @@ exports.updateEmployee = function(empId,empName,empAddress,empAccount,email,mana
   });
 }
 
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb+srv://EmployeeEngagement:Hackathon2020@cluster0.2txht.azure.mongodb.net/"
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("EmployeeEngagement");
+
+//   var Appreciation = [
+//     { EmpId: '2370004', Appreciation_Description: '', Wishes: '', Gift: '', ManagerComment:''},
+//     { EmpId: '2361353', Appreciation_Description: '', Wishes: '', Gift: '', ManagerComment:''},
+//   ];
+// //   var Employees = [
+// //     { EmpId: '2370004', EmpName: 'Ashish Mehta', EmpAddress: 'Delhi', EmpAccount: 'FRB', Email:'ashish.mehta@mphasis.com', Manager: 'Brijesh Tiwari', ManagerEmail:'brijesh.t@mphasis.com',Contact: '9028596846', Team: 'Digital Technology', Organization: 'Mphasis'},
+// //     { EmpId: '2361353', EmpName: 'Rohit Jaiswal', EmpAddress: 'Delhi', EmpAccount: 'FRB', Email:'rohit.jaiswal@mphasis.com', Manager: 'Brijesh Tiwari', ManagerEmail:'brijesh.t@mphasis.com', Contact: '7073723149', Team: 'Digital Technology', Organization: 'Mphasis'},
+// //   ];
+//    dbo.collection("Appreciation").find({}).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//    });
+//   dbo.collection("Appreciation").insertMany(Appreciation, function(err, res) {
+//     if (err) throw err;
+//     console.log("Number of documents inserted: " + res.insertedCount);
+//     db.close();
+//   });
+// });
+
+
+
--- src/service/NotifyService.js
+++ src/service/NotifyService.js
@@ -1,5 +1,13 @@
 'use strict';
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'rohitjaizz666@gmail.com',
+      pass: 'Badmorning@123'
+    }
+});  
 
 /**
  * Send notification to  user
@@ -13,7 +21,34 @@
  **/
 exports.notify = function(empId,wishes,gift,appreciationDescription) {
   return new Promise(function(resolve, reject) {
-    resolve();
+      var MongoClient = require('mongodb').MongoClient;
+      var url = "mongodb+srv://EmployeeEngagement:Hackathon2020@cluster0.2txht.azure.mongodb.net/"
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("EmployeeEngagement");
+        dbo.collection("EmployeeNew").find({"EmpId":'2370004'}).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          var mailOptions = {
+            from: 'FRBEmployeeAppreciation@mphasis.com',
+            to: `${result[0].ManagerEmail}, ${result[0].Email}`,
+            subject: 'Congratulation !!! New Appreciation Recived',
+            text: appreciationDescription
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              reject(error);
+            } else {
+              let response = {
+                "payload": 'Email sent: ' + info.response
+              }
+              resolve(response);
+            }
+          });
+          db.close();
+        });
+      });
   });
 }
 
