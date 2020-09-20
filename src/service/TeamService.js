dccf8f7fd5155bcf2bd6ce23c88f1bcdcb9dcc51
Rohit Jaiswal <rohit1952425@gmail.com>
2020-09-20 23:43:55 +0530

    Add files via upload

---
+++ src/service/TeamService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * Set Flagging for Team Level appriciation
+ * Set Flagging for Team  Appriaciation 
+ *
+ * no response value expected for this operation
+ **/
+exports.teamFlagging = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
