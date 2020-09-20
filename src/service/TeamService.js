230c3c822b1bd24d5db8a5d01b23ed9a8952e8a8
Rohit Jaiswal <rohit1952425@gmail.com>
2020-09-21 00:28:27 +0530

    Update TeamService.js

--- src/service/TeamService.js
+++ src/service/TeamService.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 /**
  * Set Flagging for Team Level appriciation
  * Set Flagging for Team  Appriaciation 
@@ -9,7 +7,52 @@
  **/
 exports.teamFlagging = function() {
   return new Promise(function(resolve, reject) {
-    resolve();
-  });
+	var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://EmployeeEngagement:Hackathon2020@cluster0.2txht.azure.mongodb.net/"
+MongoClient.connect(url, function(err, db) {
+if (err) throw err;
+var dbo = db.db("EmployeeEngagement");
+let teams = {'Digital Technology': 100, 'Deposits': 100, 'Lending': 100, 'Digital PWM': 100, 'RCT': 100}
+let responseJson=[];
+let counter=0;
+Object.keys(teams).forEach(team => {
+     dbo.collection("EmployeeNew").find({"Team":team}).toArray ( 
+        function(err, result) {
+        if (err) throw err;
+        let countter = 0
+        counter++
+        result.forEach(record => {
+            countter += countter + record.AppriciationCounter
+        })
+        let percentile= parseInt((countter/teams[team])*100)
+        if(countter >= 50){
+            responseJson.push({
+              Team: team,
+              Color: 'Green',
+              Percentile: percentile
+            })
+        }
+         else if(countter >= 30 && countter < 50){
+            responseJson.push({
+              Team: team,
+              Color: 'Yellow',
+              Percentile: percentile
+            })
+        } else {
+            responseJson.push({
+              Team: team,
+              Color: 'Red',
+              Percentile: percentile
+            })
+        }
+        if(counter==Object.keys(teams).length){
+          resolve(responseJson)
+        }
+        db.close();
+    });
+})
+});
+});
 }
+
 
